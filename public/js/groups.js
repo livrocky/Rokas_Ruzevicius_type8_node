50822d4fbecedf920cba8c022a88bf1bb4f46af9
rokas <rokasra@gmail.com>
2022-05-28 15:28:02 +0300

    groupArr logged

--- public/js/groups.js
+++ public/js/groups.js
@@ -5,27 +5,23 @@ const cardContainerEl = document.querySelector('.cards-container');
 function createCard(newCardObj) {
   const articleEl = document.createElement('article');
 
-  articleEl.className = 'card-service';
-  makeEl('h3', `${newCardObj.numberplates}`, articleEl);
-  makeEl('p', `${newCardObj.title}`, articleEl);
+  articleEl.className = 'card-group';
+  makeEl('h3', `${newCardObj.id}`, articleEl);
+  makeEl('p', `${newCardObj.name}`, articleEl);
 
   return articleEl;
 }
 
 async function getGroups(token) {
   try {
-    const groupsArr = await getFetch('accounts', token);
+    const groupsArr = await getFetch('groups', token);
     console.log('groupsArr===', groupsArr);
     if (!Array.isArray(groupsArr)) {
-      alert('Your session has expired!');
-      window.location.href = 'login.html';
+      console.log('Your session has expired!');
+      //   window.location.href = 'login.html';
     }
-
-    const groupsArr = await resp.json();
-    console.log('groupsArr ===', groupsArr);
-    console.log('piesiam korteles');
     renderGroups(groupsArr, cardContainerEl);
-    // createCard(servicesArr[0]);
+    createCard(groupsArr[0]);
   } catch (error) {
     console.warn('error ===', error);
     console.log('atvaizduojam klaida');
